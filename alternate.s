7b8f1a9a37561ce72c1f6d727d14b44d9266ce92
Aaron Walsh <ledzeppelindrummer@gmail.com>
2020-03-29 17:23:43 -0400

    Cleaned up assembly code and added comments

--- alternate.s
+++ alternate.s
@@ -1,31 +1,41 @@
+; 2020 Aaron Walsh
+; Alternating bits on PORTA of the 65C22
 
+; 65C22 address locations
+; memory map:
+; $0000 - $6000 unused, will be future RAM location
+; $6000 - $7FFF WDC65C22 address location
+; $8000 - $FFFF - AT28C256 ROM location
+; $FFFC - $FFFD - initialization vector
+
+; setup WDC65C22 memory mapping
 PORTA = $6001
 PORTB = $6000
-DDRB = $6002
-DDRA = $6003
+DDRB  = $6002
+DDRA  = $6003
 
   .org $8000
 
 reset:
   lda #$ff
   sta DDRB 
-  ; set all PORTB to full output
+      ; set all PORTB to full output
   
   lda #$55
   sta PORTB
-  ; set alternating bits on PORTB  
+      ; set alternating bits on PORTB  
 
 loop:
   lda #$aa
   sta PORTB
-  ; set alternating bits on PORTB
+      ; set alternating bits on PORTB
 
   lda #$55
   sta PORTB
-  ; set alternating bits on PORTB
+      ; set alternating bits on PORTB
   jmp loop
 
-  ; insert reset vector
+      ; insert reset vector
   .org $fffc
   .word reset
   .word $0000
